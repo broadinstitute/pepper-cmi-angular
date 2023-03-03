06303b7514923400b9effa6e38a7f90b7fd30064
Andrew Zimmer <andrew@broadinstitute.org>
2023-03-03 09:58:20 -0500

    WIP

--- ddp-workspace/projects/toolkit/src/lib/components/footer/footer.component.ts
+++ ddp-workspace/projects/toolkit/src/lib/components/footer/footer.component.ts
@@ -7,7 +7,7 @@ import { AnalyticsEventsService, WindowRef, AnalyticsEventCategories, NGXTransla
 import { Observable } from 'rxjs';
 import { map, mergeMap } from 'rxjs/operators';
 
-// tt
+// ttt
 
 @Component({
     selector: 'toolkit-footer',
